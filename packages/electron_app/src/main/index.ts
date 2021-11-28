0e35311964f191b9da6de59dc9e8722a2a332d60
Aidan Petti <apetti1920@aol.com>
2021-11-28 11:10:52 -0700

    integrated web and electron

---
+++ packages/electron_app/src/main/index.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "..", "renderer", "index.html"))
+        .then(() => console.log("Loaded Webpage"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
