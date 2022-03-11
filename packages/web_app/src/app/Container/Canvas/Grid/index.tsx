ba891203e450e38480f820a2d65db42a96e20b42
Aidan Petti <apetti1920@aol.com>
2022-03-11 00:36:30 -0700

    recursive grid zoom working (kind of)

--- packages/web_app/src/app/Container/Canvas/Grid/index.tsx
+++ packages/web_app/src/app/Container/Canvas/Grid/index.tsx
@@ -2,6 +2,59 @@ import React from "react";
 
 import { PointType } from '@compx/common/Types'
 
+function MicroGrid(props: { size: number, scale: number, translation: PointType, opacity: number }): React.ReactElement {
+    const smallGridSize1 = (props.size*props.scale);
+    const largeGridSize1 = smallGridSize1 * 10;
+
+    return (
+        <React.Fragment>
+            <defs>
+                <pattern id="smallGrid1" width={smallGridSize1} height={smallGridSize1} patternUnits="userSpaceOnUse">
+                    <path d={`M ${smallGridSize1} 0 L 0 0 0 ${smallGridSize1}`} fill="none" stroke="gray"
+                          strokeWidth="0.5" opacity={props.opacity}/>
+                </pattern>
+                <pattern id="grid1" width={largeGridSize1} height={largeGridSize1} patternUnits="userSpaceOnUse"
+                         patternTransform={`translate(${props.translation.x} ${props.translation.y})`}>
+                    <rect width={largeGridSize1} height={largeGridSize1} fill="url(#smallGrid1)"/>
+                    <path d={`M ${largeGridSize1} 0 L 0 0 0 ${largeGridSize1}`} fill="none" stroke="gray"
+                          strokeWidth="1" opacity={props.opacity}/>
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#grid1)" />
+        </React.Fragment>
+    )
+}
+
+function RecursiveMicroGrid(props: { size: number, scale: number, translation: PointType, level?: number}): React.ReactElement {
+    const turnPoint = 3;
+    const cvtLevel = props.level ?? 0;
+    const cvtScale = props.scale - (turnPoint*cvtLevel);
+
+    if (cvtScale < turnPoint) {
+        return (
+            <MicroGrid size={props.size} scale={props.scale}
+                       translation={props.translation} opacity={1} />
+        )
+    } else if (cvtScale < turnPoint + 1) {
+        const pct = (turnPoint+1)-cvtScale;
+        return (
+            <React.Fragment>
+                <MicroGrid size={props.size/10} scale={props.scale}
+                           translation={props.translation}
+                           opacity={1-pct} />
+                <MicroGrid size={props.size} scale={props.scale}
+                           translation={props.translation}
+                           opacity={pct} />
+            </React.Fragment>
+        )
+    } else {
+        return (
+            <RecursiveMicroGrid size={props.size/10} scale={props.scale}
+                                translation={props.translation} level={cvtLevel+1} />
+        )
+    }
+}
+
 type PropType = {
     canvasTranslation: PointType
     canvasZoom: number
@@ -11,77 +64,24 @@ type PropType = {
 };
 
 type StateType = {
-    oldZoom: number,
-    level: number
+    oldZoom: number
 };
 
 export default class Grid extends React.Component<PropType, StateType> {
-    private static readonly SwitchPoint = (4.0 - (1/3)) / 4;
-
     constructor(props: PropType) {
         super(props);
 
         this.state = {
-            oldZoom: this.props.canvasZoom,
-            level: Math.floor((props.canvasZoom - (1/3)) / (4.0 - (1/3)))
-        }
-    }
-
-    static getDerivedStateFromProps(newProps: PropType, oldState: StateType): StateType {
-        const oz = (oldState.oldZoom % Grid.SwitchPoint) / Grid.SwitchPoint;
-        const nz = (newProps.canvasZoom % Grid.SwitchPoint) / Grid.SwitchPoint;
-
-        const newState: StateType = {...oldState, oldZoom: newProps.canvasZoom };
-        if (oz >= 0.98 && nz <= 0.02) {
-            console.log("State up");
-            return {...newState, level: oldState.level += 1}
-        } else if (oz <= 0.02 && nz >= 0.98) {
-            console.log("State down");
-            return {...newState, level: oldState.level -= 1}
-        } else {
-            return newState;
+            oldZoom: this.props.canvasZoom
         }
     }
 
     render() {
-        // let opp1 = 1 - ((this.props.canvasZoom % Grid.SwitchPoint) / Grid.SwitchPoint);
-        // opp1 = opp1 <= 0.25 ? 0 : (opp1 >= 0.75 ? 1.0 : opp1)
-        // const opp2 = 1-opp1;
-
-        const smallGridSize1 = 10 * (10**this.state.level);
-        const largeGridSize1 = smallGridSize1 * 10
-
-        // const smallGridSize2 = 150 + (150 * (this.state.level + 1));
-        // const largeGridSize2 = smallGridSize2 * 10;
-
         return (
             <g id="BackgroundGrid" width="100%" height="100%" order={1} pointerEvents="none">
-                <defs>
-                    <pattern id="smallGrid1" width={smallGridSize1} height={smallGridSize1} patternUnits="userSpaceOnUse">
-                        <path d={`M ${smallGridSize1} 0 L 0 0 0 ${smallGridSize1}`} fill="none" stroke="gray" strokeWidth="0.5"/>
-                    </pattern>
-                    <pattern id="grid1" width={largeGridSize1} height={largeGridSize1} patternUnits="userSpaceOnUse"
-                             patternTransform={`translate(${this.props.canvasTranslation.x} ${this.props.canvasTranslation.y}) 
-                                                                    scale(${this.props.canvasZoom} ${this.props.canvasZoom})`}>
-                        <rect width={largeGridSize1} height={largeGridSize1} fill="url(#smallGrid1)"/>
-                        <path d={`M ${largeGridSize1} 0 L 0 0 0 ${largeGridSize1}`} fill="none" stroke="gray" strokeWidth="1"/>
-                    </pattern>
+                <RecursiveMicroGrid size={40} scale={this.props.canvasZoom} translation={this.props.canvasTranslation}/>
 
-                    {/*<pattern id="smallGrid2" width={smallGridSize2} height={smallGridSize2} patternUnits="userSpaceOnUse">*/}
-                    {/*    <path d={`M ${smallGridSize2} 0 L 0 0 0 ${smallGridSize2}`} fill="none" stroke="gray" strokeWidth="0.5"/>*/}
-                    {/*</pattern>*/}
-                    {/*<pattern id="grid2" width={largeGridSize2} height={largeGridSize2} patternUnits="userSpaceOnUse"*/}
-                    {/*         patternTransform={`translate(${this.props.canvasTranslation.x} ${this.props.canvasTranslation.y}) */}
-                    {/*                                                scale(${this.props.canvasZoom} ${this.props.canvasZoom})`}>*/}
-                    {/*    <rect width={largeGridSize2} height={largeGridSize2} fill="url(#smallGrid2)" opacity={`${opp2*100}%`}/>*/}
-                    {/*    <path d={`M ${largeGridSize2} 0 L 0 0 0 ${largeGridSize2}`} fill="none" stroke="gray" strokeWidth="1" opacity={`${opp2*100}%`}/>*/}
-                    {/*</pattern>*/}
-                </defs>
-
-                <rect width="100%" height="100%" fill="url(#grid1)" />
-                {/*<rect width="100%" height="100%" fill="url(#grid2)" />*/}
-                <g transform={`translate(${this.props.canvasTranslation.x} ${this.props.canvasTranslation.y}) 
-                                                              scale(${this.props.canvasZoom} ${this.props.canvasZoom})`}>
+                <g>
                     <path d={`M 0 0 L 0 ${-80 / this.props.canvasZoom}`} fill='none' stroke="red" strokeWidth={1}/>
                     <path d={`M 0 0 L ${80 / this.props.canvasZoom} 0`} fill='none' stroke="green" strokeWidth={1}/>
                 </g>
